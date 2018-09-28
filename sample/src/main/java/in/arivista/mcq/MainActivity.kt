de6040c162aef894f9f3ec26cb1f9e178a019a87
karthee <karthegeyan.kumaragurubaran@arivistadigital.com>
2018-09-28 14:59:42 +0530

    created Sample using Library

--- sample/src/main/java/in/arivista/mcq/MainActivity.kt
+++ sample/src/main/java/in/arivista/mcq/MainActivity.kt
@@ -1,12 +1,38 @@
 package `in`.arivista.mcq
 
-import android.support.v7.app.AppCompatActivity
+import `in`.arivista.mcq.mcq.choiceview.ArivistaCustomView
+import `in`.arivista.mcq.mcq.model.ChoiceModel
+import `in`.arivista.mcq.mcq.utils.ChoiceType
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+            setContentView(R.layout.activity_main)
+
+
+            val choicesList = ArrayList<ChoiceModel>()
+            choicesList.add(ChoiceModel(1, "Option 1", true))
+            choicesList.add(ChoiceModel(2, "Option 2", false))
+            choicesList.add(ChoiceModel(3, "Option 3", false))
+            choicesList.add(ChoiceModel(4, "Option 4", false))
+
+            val choicesList1 = ArrayList<ChoiceModel>()
+            choicesList1.add(ChoiceModel(1, "Option 1", true))
+            choicesList1.add(ChoiceModel(2, "Option 2", false))
+            choicesList1.add(ChoiceModel(3, "Option 3", true))
+            choicesList1.add(ChoiceModel(4, "Option 4", false))
+
+            val arivistaview = findViewById<ArivistaCustomView>(R.id.arivista_view)
+
+            arivistaview.setQuestion("how to implement Radio Buttons?");
+            arivistaview.setChoiceType(choicesList, ChoiceType.SINGLE)
+
+            arivistaview.setQuestion("how to implement Radio CheckedBox?");
+            arivistaview.setChoiceType(choicesList1, ChoiceType.MULTIPLE)
+
+
     }
 }
