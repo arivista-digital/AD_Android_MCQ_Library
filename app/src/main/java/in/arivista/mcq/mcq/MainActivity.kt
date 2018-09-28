cf043467ac7f52f734ccfcc985de06fa3a222eb6
Suresh <suresh.seeni@arivistadigital.com>
2018-09-28 14:44:18 +0530

    MainActivity Recommit

--- app/src/main/java/in/arivista/mcq/mcq/MainActivity.kt
+++ app/src/main/java/in/arivista/mcq/mcq/MainActivity.kt
@@ -13,6 +13,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        val choicesList = ArrayList<ChoiceModel>()
+        choicesList.add(ChoiceModel(1, "Option 1", true))
+        choicesList.add(ChoiceModel(2, "Option 2", false))
+        choicesList.add(ChoiceModel(3, "Option 3", false))
+        choicesList.add(ChoiceModel(4, "Option 4", false))
+
+        val choicesList1 = ArrayList<ChoiceModel>()
+        choicesList1.add(ChoiceModel(1, "Option 1", true))
+        choicesList1.add(ChoiceModel(2, "Option 2", false))
+        choicesList1.add(ChoiceModel(3, "Option 3", true))
+        choicesList1.add(ChoiceModel(4, "Option 4", false))
+
+        val arivista_view = findViewById(R.id.arivista_view) as Arivista_Custom_View
+
+        arivista_view.setQuestion("how to implement Radio Buttons?");
+        arivista_view.setChoiceType(choicesList, ChoiceType.SINGLE)
+
+        arivista_view.setQuestion("how to implement Radio CheckedBox?");
+        arivista_view.setChoiceType(choicesList1, ChoiceType.MULTIPLE)
+
     }
 
 }
